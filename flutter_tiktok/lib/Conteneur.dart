d32e795fcc74b74effd2d03dedb261cee85b3f3d
Aboubacar Koné <aboubacar.kone@odc.local>
2021-11-16 19:41:07 +0000

    commit

--- flutter_tiktok/lib/Conteneur.dart
+++ flutter_tiktok/lib/Conteneur.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 class Conteneur extends StatelessWidget {
    Conteneur({Key? key}) : super(key: key);
 
@@ -82,12 +82,11 @@ class Conteneur extends StatelessWidget {
                         alignment: AlignmentDirectional.bottomCenter,
                         children: [
                           Container(
+                            margin: EdgeInsets.only(bottom: 10),
                             child: CircleAvatar(
                                 radius:30,
                                 backgroundImage: AssetImage('assets/images/Abou.jpeg'),
                           ),
-                            margin: EdgeInsets.only(bottom: 10),
-
                           ),
                           Container(
                             padding: const EdgeInsets.all(2),
@@ -104,22 +103,69 @@ class Conteneur extends StatelessWidget {
                         ],
                       ),
                     ),
-                    Container(
+                    SizedBox(
                       height: 80,
-                      color: Colors.teal,
+                      //color: Colors.teal,
+                      child:Column(
+                        children:[
+                          Icon(
+                            Icons.favorite,
+                            color:Colors.white.withOpacity(0.85),
+                            size:45,
+                          ),
+                        const Text(
+                            '980.8K',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                        ),
+                        ]
+                      )
                     ),
                     Container(
                       height: 80,
-                      color: Colors.yellow,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.comment,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          const Text(
+                            '789',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                     Container(
                       height: 80,
-                      color: Colors.orange,
+                      //color: Colors.orange
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.share,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          const Text(
+                            '897',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    Container(
-                      height: 80,
-                      color: Colors.purple,
-                    ),
+                    const AnimatedLogo(),
                   ],
                 ),
               )
@@ -127,6 +173,57 @@ class Conteneur extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+class AnimatedLogo extends StatefulWidget {
+  const AnimatedLogo({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedLogoState createState() => _AnimatedLogoState();
+}
+class _AnimatedLogoState extends State<AnimatedLogo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 4000),
+      vsync: this,
+    );
+    _controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (_, child) {
+        return Transform.rotate(
+          angle: _controller.value * 2 * math.pi,
+          child: child,
+        );
+      },
+      child: Container(
+        height: 45,
+        width: 45,
+        padding: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          image: const DecorationImage(
+            image: AssetImage("assets/images/disc_icon.png"),
+          ),
+        ),
+        child: Image.asset('assets/images/tiktok_icon.png'),
+      ),
     );
   }
 }
